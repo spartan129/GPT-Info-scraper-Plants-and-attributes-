8c37b05d0d23502e4ee6aba1cf66a0471e7b9b1a
spartan129 <128940775+spartan129@users.noreply.github.com>
2023-04-18 07:49:08 -0400

    Update plant_info_extractor.py
    Updated to gpt 3.5 turbo, making it 10x cheaper to run per request with higher prompt configurability.

--- plant_info_extractor.py
+++ plant_info_extractor.py
@@ -1,90 +1,59 @@
-#3.1.4
-# Import necessary libraries
+#3.2.4
 import openai
 import csv
 import os
 import re
 import requests
+from dotenv import load_dotenv
 
-# Set OpenAI API key and organization
-openai.api_key = "OPENAI_API_KEY"
-openai.organization = "YOUR_ORG_ID"
+load_dotenv()
 
-# Function to retrieve plant details using GPT-4
-def get_plant_details(plant_name):
-    # This model can be changed for easier or harder tasks
-    model_engine = "text-davinci-002"
-    prompt = (f"Provide detailed plant attributes for {plant_name} in a clear and structured format without any extra labels. "
-              f"Include the following attributes, separated by semicolons: "
-              f"Height, Spread, Soil, Sunlight, Bloom Time, Flower Color, Fragrance, and Hardiness Zones.")
+openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.organization = os.getenv("OPENAI_ORG_ID")
 
-    # Send API request, handle timeouts, and parse the response
-    try:
-        response = openai.Completion.create(engine=model_engine, prompt=prompt, max_tokens=1024, n=1, stop=None, temperature=0.7, timeout=10)
-    except requests.exceptions.ReadTimeout:
-        print(f"Timeout reached for {plant_name}. Skipping.")
-        return None
+def clean_plant_name(plant_name):
+    plant_name = re.sub(r'\s+\d+(\.\d+)?\s?(CAL|G|IN)', '', plant_name)
+    return plant_name
 
-    response = openai.Completion.create(engine=model_engine, prompt=prompt, max_tokens=1024, n=1, stop=None, temperature=0.7)
-
-    details = response.choices[0].text.strip()
-
-    # Extract and store plant attributes in a dictionary
-    attributes = {
-        "Height": convert_units(extract_attribute(r"height:?\s*((?:\d+-)?\d+\s*(?:ft|feet|inches|inch|in|\')?)", details)),
-        "Spread": convert_units(extract_attribute(r"spread:?\s*((?:\d+-)?\d+\s*(?:ft|feet|inches|inch|in|\')?)", details)),
-        "Soil": extract_attribute(r"soil:?\s*([\w\s-]+)", details),
-        "Sunlight": extract_attribute(r"sunlight:?\s*([\w\s-]+)", details),
-        "Bloom Time": extract_attribute(r"bloom time:?\s*([\w\s-]+)", details),
-        "Flower Color": extract_attribute(r"flower color:?\s*([\w\s-]+)", details),
-        "Fragrance": extract_attribute(r"fragrance:?\s*([\w\s-]+)", details),
-        "Hardiness Zones": extract_attribute(r"hardiness zones:?\s*([\d\s-]+)", details),
-    }
-
-    return attributes
-
-# Function to convert units in height and spread attributes
 def convert_units(value):
     value = value.strip().lower()
     value = re.sub(r"\s*feet|\s*ft'", " ft", value)
     value = re.sub(r"\s*inches|\s*inch|\s*in'", " in", value)
     return value
 
-# Function to extract an attribute from the text using regex
-def extract_attribute(pattern, text):
-    match = re.search(pattern, text, re.IGNORECASE)
-    if match:
-        return match.group(1).strip()
-    else:
-        return ""
-
-# Function to clean plant names by removing size indicators
-def clean_plant_name(plant_name):
-    plant_name = re.sub(r'\s+\d+(\.\d+)?\s?(CAL|G|IN)', '', plant_name)
-    return plant_name
-
-# Main function to read plant names, retrieve their information, and write to a CSV file
 def main():
     plants_file = open('plants.txt', 'r')
-    plants = [clean_plant_name(line.strip()) for line in plants_file.readlines()]
+    plants = [(line.strip(), clean_plant_name(line.strip())) for line in plants_file.readlines()]
 
-    # Create and write to the CSV file
     with open('plant_info.csv', mode='w', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow(["Plant Name", "Height", "Spread", "Soil", "Sunlight", "Bloom Time", "Flower Color", "Fragrance", "Hardiness Zones"])
+        writer.writerow(["Plant Name", "Hardiness Zone", "Light Requirement", "Water Requirement", "Growth Rate", "Growth Habit", "Max Height", "Max Width", "Flower Color", "Flowering Season", "Tolerances", "Botanical Name"])
 
-        # Loop through plant names, get their details, and write to the CSV file
-        for plant in plants:
+        for original_plant, cleaned_plant in plants:
             try:
-                info = get_plant_details(plant)
-                print(f"\n{plant} Plant Attributes:")
-                for key, value in info.items():
-                    print(f"{key}: {value}")
-                writer.writerow([plant, info["Height"], info["Spread"], info["Soil"], info["Sunlight"], info["Bloom Time"], info["Flower Color"], info["Fragrance"], info["Hardiness Zones"]])
-                print(f"Got information for {plant}")
-            except Exception as e:
-                print(f"Error getting information for {plant}: {str(e)}")
+                model_engine = "gpt-3.5-turbo"
+                prompt = (f"please return, without any other text, concise plant attributes/information with only the following attributes in a csv form. Hardiness Zone, Light Requirement, Water Requirement, Growth Rate, Growth Habit, Max Height, Max Width, Flower Color, Flowering Season, Tolerances and Botanical name for the following plant: {cleaned_plant}.Please include all attributes in the output, separated by commas. If an attribute has no value, use a comma with no value next to it. For attributes with multiple values, separate them using a dash (-) instead of a comma.")
 
-# Run the main function if the script is executed
+                response = openai.ChatCompletion.create(
+                    model=model_engine,
+                    messages=[{"role": "system", "content": "You are a helpful assistant that provides information about plants."},
+                      {"role": "user", "content": prompt}],
+                    max_tokens=1024,
+                    n=1,
+                    temperature=0.6
+                )
+                print(response)
+                details = response.choices[0].message['content'].strip().split(',')
+
+                max_height = convert_units(details[5].strip())
+                max_width = convert_units(details[6].strip())
+
+                details = [details[i].strip() for i in range(5)] + [max_height, max_width] + [details[i].strip() for i in range(7, 11)]
+
+                writer.writerow([original_plant] + details)
+                print(f"Got information for {original_plant}")
+            except Exception as e:
+                print(f"Error getting information for {original_plant}: {str(e)}")
+
 if __name__ == "__main__":
     main()
